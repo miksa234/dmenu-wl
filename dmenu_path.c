4391c090e41e5454a4f7bd48c7115fd785340eb4
Milutin Popovic <milutin@popovic.xyz>
2026-09-06 15:19:14 +0100

    format

--- dmenu_path.c
+++ dmenu_path.c
@@ -9,89 +9,99 @@
 
 #define CACHE ".cache/dmenu_run"
 
-static void die(const char *s);
-static int qstrcmp(const void *a, const void *b);
-static void scan(void);
-static int uptodate(void);
+static void die (const char *s);
+static int qstrcmp (const void *a, const void *b);
+static void scan (void);
+static int uptodate (void);
 
 static char **items = NULL;
 static const char *home, *path;
 
-int main(void) {
-  if (!(home = getenv("HOME")))
-    die("no $HOME");
-  if (!(path = getenv("PATH")))
-    die("no $PATH");
-  if (chdir(home) < 0)
-    die("chdir failed");
-  if (uptodate()) {
-    execlp("cat", "cat", CACHE, NULL);
-    die("exec failed");
-  }
-  scan();
-  return EXIT_SUCCESS;
-}
-
-void die(const char *s) {
-  fprintf(stderr, "dmenu_path: %s\n", s);
-  exit(EXIT_FAILURE);
-}
-
-int qstrcmp(const void *a, const void *b) {
-  return strcmp(*(const char **)a, *(const char **)b);
-}
-
-void scan(void) {
-  char buf[PATH_MAX];
-  char *dir, *p;
-  size_t i, count;
-  struct dirent *ent;
-  DIR *dp;
-  FILE *cache;
-
-  count = 0;
-  if (!(p = strdup(path)))
-    die("strdup failed");
-  for (dir = strtok(p, ":"); dir; dir = strtok(NULL, ":")) {
-    if (!(dp = opendir(dir)))
-      continue;
-    while ((ent = readdir(dp))) {
-      snprintf(buf, sizeof buf, "%s/%s", dir, ent->d_name);
-      if (ent->d_name[0] == '.' || access(buf, X_OK) < 0)
-        continue;
-      if (!(items = realloc(items, ++count * sizeof *items)))
-        die("malloc failed");
-      if (!(items[count - 1] = strdup(ent->d_name)))
-        die("strdup failed");
+int
+main (void)
+{
+    if (!(home = getenv ("HOME")))
+        die ("no $HOME");
+    if (!(path = getenv ("PATH")))
+        die ("no $PATH");
+    if (chdir (home) < 0)
+        die ("chdir failed");
+    if (uptodate ()) {
+        execlp ("cat", "cat", CACHE, NULL);
+        die ("exec failed");
     }
-    closedir(dp);
-  }
-  qsort(items, count, sizeof *items, qstrcmp);
-  if (!(cache = fopen(CACHE, "w")))
-    die("open failed");
-  for (i = 0; i < count; i++) {
-    if (i > 0 && !strcmp(items[i], items[i - 1]))
-      continue;
-    fprintf(cache, "%s\n", items[i]);
-    fprintf(stdout, "%s\n", items[i]);
-  }
-  fclose(cache);
-  free(p);
+    scan ();
+    return EXIT_SUCCESS;
 }
 
-int uptodate(void) {
-  char *dir, *p;
-  time_t mtime;
-  struct stat st;
+void
+die (const char *s)
+{
+    fprintf (stderr, "dmenu_path: %s\n", s);
+    exit (EXIT_FAILURE);
+}
 
-  if (stat(CACHE, &st) < 0)
-    return 0;
-  mtime = st.st_mtime;
-  if (!(p = strdup(path)))
-    die("strdup failed");
-  for (dir = strtok(p, ":"); dir; dir = strtok(NULL, ":"))
-    if (!stat(dir, &st) && st.st_mtime > mtime)
-      return 0;
-  free(p);
-  return 1;
+int
+qstrcmp (const void *a, const void *b)
+{
+    return strcmp (*(const char **)a, *(const char **)b);
+}
+
+void
+scan (void)
+{
+    char buf[PATH_MAX];
+    char *dir, *p;
+    size_t i, count;
+    struct dirent *ent;
+    DIR *dp;
+    FILE *cache;
+
+    count = 0;
+    if (!(p = strdup (path)))
+        die ("strdup failed");
+    for (dir = strtok (p, ":"); dir; dir = strtok (NULL, ":")) {
+        if (!(dp = opendir (dir)))
+            continue;
+        while ((ent = readdir (dp))) {
+            snprintf (buf, sizeof buf, "%s/%s", dir, ent->d_name);
+            if (ent->d_name[0] == '.' || access (buf, X_OK) < 0)
+                continue;
+            if (!(items = realloc (items, ++count * sizeof *items)))
+                die ("malloc failed");
+            if (!(items[count - 1] = strdup (ent->d_name)))
+                die ("strdup failed");
+        }
+        closedir (dp);
+    }
+    qsort (items, count, sizeof *items, qstrcmp);
+    if (!(cache = fopen (CACHE, "w")))
+        die ("open failed");
+    for (i = 0; i < count; i++) {
+        if (i > 0 && !strcmp (items[i], items[i - 1]))
+            continue;
+        fprintf (cache, "%s\n", items[i]);
+        fprintf (stdout, "%s\n", items[i]);
+    }
+    fclose (cache);
+    free (p);
+}
+
+int
+uptodate (void)
+{
+    char *dir, *p;
+    time_t mtime;
+    struct stat st;
+
+    if (stat (CACHE, &st) < 0)
+        return 0;
+    mtime = st.st_mtime;
+    if (!(p = strdup (path)))
+        die ("strdup failed");
+    for (dir = strtok (p, ":"); dir; dir = strtok (NULL, ":"))
+        if (!stat (dir, &st) && st.st_mtime > mtime)
+            return 0;
+    free (p);
+    return 1;
 }
--- draw.h
+++ draw.h
@@ -18,119 +18,119 @@ enum { ColBG, ColFG, ColBorder, ColLast };
 struct dmenu_panel;
 
 enum dmenu_position {
-  DMENU_POSITION_CENTER,
-  DMENU_POSITION_TOP,
-  DMENU_POSITION_BOTTOM
+    DMENU_POSITION_CENTER,
+    DMENU_POSITION_TOP,
+    DMENU_POSITION_BOTTOM
 };
 
 struct draw_buffer {
-  struct dmenu_panel *panel;
-  cairo_t *cairo;
-  cairo_surface_t *cairo_surface;
-  struct wl_buffer *buffer;
-  void *data;
-  size_t size;
-  int32_t width;
-  int32_t height;
-  bool busy;
+    struct dmenu_panel *panel;
+    cairo_t *cairo;
+    cairo_surface_t *cairo_surface;
+    struct wl_buffer *buffer;
+    void *data;
+    size_t size;
+    int32_t width;
+    int32_t height;
+    bool busy;
 };
 
 struct monitor_info {
-  uint32_t registry_name;
-  int32_t physical_width;
-  int32_t physical_height;
-  int32_t logical_width;
-  int32_t logical_height;
-  int32_t scale;
+    uint32_t registry_name;
+    int32_t physical_width;
+    int32_t physical_height;
+    int32_t logical_width;
+    int32_t logical_height;
+    int32_t scale;
 
-  char name[MAX_MONITOR_NAME_LEN];
+    char name[MAX_MONITOR_NAME_LEN];
 
-  enum wl_output_subpixel subpixel;
+    enum wl_output_subpixel subpixel;
 
-  struct wl_output *output;
-  struct dmenu_panel *panel;
-  struct monitor_info *next;
+    struct wl_output *output;
+    struct dmenu_panel *panel;
+    struct monitor_info *next;
 };
 
 struct display_info {
-  struct wl_display *display;
-  struct wl_registry *registry;
-  struct wl_compositor *compositor;
-  struct wl_seat *seat;
-  uint32_t seat_registry_name;
-  struct wp_fractional_scale_manager_v1 *fractional_scale_manager;
-  struct wp_viewporter *viewporter;
-  struct monitor_info *monitors;
+    struct wl_display *display;
+    struct wl_registry *registry;
+    struct wl_compositor *compositor;
+    struct wl_seat *seat;
+    uint32_t seat_registry_name;
+    struct wp_fractional_scale_manager_v1 *fractional_scale_manager;
+    struct wp_viewporter *viewporter;
+    struct monitor_info *monitors;
 };
 
 struct keyboard_info {
-  struct wl_keyboard *kbd;
-  struct xkb_context *xkb_context;
-  struct xkb_keymap *xkb_keymap;
-  struct xkb_state *xkb_state;
-  bool control;
-  bool shift;
+    struct wl_keyboard *kbd;
+    struct xkb_context *xkb_context;
+    struct xkb_keymap *xkb_keymap;
+    struct xkb_state *xkb_state;
+    bool control;
+    bool shift;
 };
 
 struct surface {
-  struct wl_surface *surface;
-  struct wl_shm *shm;
-  struct draw_buffer buffers[2];
-  struct zwlr_layer_shell_v1 *layer_shell;
-  struct zwlr_layer_surface_v1 *layer_surface;
-  struct wp_fractional_scale_v1 *fractional_scale;
-  struct wp_viewport *viewport;
+    struct wl_surface *surface;
+    struct wl_shm *shm;
+    struct draw_buffer buffers[2];
+    struct zwlr_layer_shell_v1 *layer_shell;
+    struct zwlr_layer_surface_v1 *layer_surface;
+    struct wp_fractional_scale_v1 *fractional_scale;
+    struct wp_viewport *viewport;
 };
 
 struct dmenu_panel {
-  struct keyboard_info keyboard;
-  /* struct monitor_info monitor; */
-  int selected_monitor;
-  char *selected_monitor_name;
+    struct keyboard_info keyboard;
+    /* struct monitor_info monitor; */
+    int selected_monitor;
+    char *selected_monitor_name;
 
-  struct monitor_info *monitor;
-  struct display_info display_info;
+    struct monitor_info *monitor;
+    struct display_info display_info;
 
-  struct surface surface;
+    struct surface surface;
 
-  void (*on_keyevent)(struct dmenu_panel *, enum wl_keyboard_key_state,
-                      xkb_keysym_t, bool, bool);
-  void (*on_keyrepeat)(struct dmenu_panel *);
+    void (*on_keyevent) (struct dmenu_panel *, enum wl_keyboard_key_state,
+                         xkb_keysym_t, bool, bool);
+    void (*on_keyrepeat) (struct dmenu_panel *);
 
-  void (*draw)(cairo_t *, int32_t, int32_t, double);
+    void (*draw) (cairo_t *, int32_t, int32_t, double);
 
-  int32_t width;
-  int32_t height;
-  enum dmenu_position position;
-  bool interactive;
-  bool configured;
-  bool closed;
-  bool redraw_pending;
-  uint32_t preferred_scale;
+    int32_t width;
+    int32_t height;
+    enum dmenu_position position;
+    bool interactive;
+    bool configured;
+    bool closed;
+    bool redraw_pending;
+    uint32_t preferred_scale;
 
-  int repeat_timer;
-  int repeat_delay;
-  int64_t repeat_period_ns;
-  enum wl_keyboard_key_state repeat_key_state;
-  xkb_keysym_t repeat_sym;
-  uint32_t repeat_key;
+    int repeat_timer;
+    int repeat_delay;
+    int64_t repeat_period_ns;
+    enum wl_keyboard_key_state repeat_key_state;
+    xkb_keysym_t repeat_sym;
+    uint32_t repeat_key;
 
-  bool running;
+    bool running;
 };
 
-void dmenu_init_panel(struct dmenu_panel *, int32_t, int32_t,
-                      enum dmenu_position, bool);
-void dmenu_draw(struct dmenu_panel *);
-void dmenu_show(struct dmenu_panel *);
-void dmenu_close(struct dmenu_panel *);
+void dmenu_init_panel (struct dmenu_panel *, int32_t, int32_t,
+                       enum dmenu_position, bool);
+void dmenu_draw (struct dmenu_panel *);
+void dmenu_show (struct dmenu_panel *);
+void dmenu_close (struct dmenu_panel *);
 
-void pango_printf(cairo_t *cairo, const char *font, double scale, bool markup,
-                  const char *fmt, ...);
-void get_text_size(cairo_t *cairo, const char *font, int *width, int *height,
-                   int *baseline, double scale, bool markup, const char *fmt,
-                   ...);
-void eprintf(const char *fmt, ...);
-void weprintf(const char *fmt, ...);
-int32_t round_to_int(double val);
+void pango_printf (cairo_t *cairo, const char *font, double scale, bool markup,
+                   const char *fmt, ...);
+void get_text_size (cairo_t *cairo, const char *font, int *width, int *height,
+                    int *baseline, double scale, bool markup, const char *fmt,
+                    ...);
+void eprintf (const char *fmt, ...);
+void weprintf (const char *fmt, ...);
+int32_t round_to_int (double val);
 
 extern const char *progname;
